404dbdeb492ead2b357cae58f625c6a910528375
FabrizioCicala <fab.cicala@gmail.com>
2018-05-10 19:35:16 +0200

    fix vari a job 2. Job ancora non funzionante!

--- src/main/java/mapReduce/job2/ScorePerYearReducer.java
+++ src/main/java/mapReduce/job2/ScorePerYearReducer.java
@@ -1,7 +1,6 @@
 package mapReduce.job2;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -10,25 +9,22 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class ScorePerYearReducer extends Reducer<Text, YearScore, Text, Text> {
 
-	private Map<Integer, LinkedList<Integer>> countMap;
-	private Map<Integer, Float> countMap2;
-
 	public void reduce (Text key, Iterable<YearScore> values,
 			Reducer<Text, YearScore, Text, Text>.Context context)
 					throws IOException, InterruptedException
 	{
-		countMap = new HashMap<>();
-		countMap2 = new HashMap<>();
+		Map<Integer, LinkedList<Integer>> countMap = new HashMap<>();
+		Map<Integer, Float> countMap2 = new HashMap<>();
 
 		LinkedList<Integer> scores = new LinkedList<>();
 		for (YearScore val : values){
-			if (countMap.containsKey(val.getYear())){
-				scores = countMap.get(val.getYear());
-				scores.add(val.getScore());
+			if (countMap.containsKey(val.getYear().get())){
+				scores = countMap.get(val.getYear().get());
+				scores.add(val.getScore().get());
 			} else
-				scores.add(val.getScore());
+				scores.add(val.getScore().get());
 		
-			countMap.put(val.getYear(), scores);
+			countMap.put(val.getYear().get(), scores);
 		
 		}
 
--- src/main/java/mapReduce/job2/YearScore.java
+++ src/main/java/mapReduce/job2/YearScore.java
@@ -1,53 +1,59 @@
 package mapReduce.job2;
 
 
-public class YearScore {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparable;
 
-	private int year;
-	private int score;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
+public class YearScore implements WritableComparable<YearScore> {
 
+	private IntWritable year;
+	private IntWritable score;
 
-	public YearScore(int y, int s) {
-		this.year = y;
-		this.score = s;  
+	/** Constructor
+     *  init class variables from IntWritable parameters
+	 **/
+    YearScore (IntWritable year, IntWritable score){
+        this.year = year;
+        this.score = score;
+    }
+
+    /** Constructor
+     *  init class variables from int parameters
+     **/
+	YearScore(int year, int score) {
+		this.year = new IntWritable(year);
+		this.score = new IntWritable(score);
 	}
 
+	/** getter methods **/
+    IntWritable getYear() { return this.year; }
 
-	public int getScore() {
-		return this.score;
-	}
+    IntWritable getScore() { return this.score;	}
 
+    /** setter methods **/
+    void setYear(int year) { this.year.set(year); }
+    void setYear(IntWritable year) { this.year = year; }
 
-	public void setScore(int score) {
-		this.score = score;
-	}
+	public void setScore(int score) { this.score.set(score); }
+    public void setScore(IntWritable score) { this.score = score; }
 
-
-	public int getYear() {
-		return this.year;
-	}
-
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-
+    /** other methods **/
 	@Override
 	public String toString() {
-		return "[score=" + this.score + ", year=" + this.year + "]";
+		return "[score=" + this.score.get() + ", year=" + this.year.get() + "]";
 	}
-
-
 
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + score;
-		result = prime * result + year;
+		result = prime * result + score.get();
+		result = prime * result + year.get();
 		return result;
 		
 		//return this.score + this.year;
@@ -55,13 +61,31 @@ public class YearScore {
 
 
     @Override
-    public boolean equals(Object o) {
-        if (o instanceof YearScore) {
-        	YearScore yearScore = (YearScore) o;
-            return (this.score == yearScore.getScore())
-                    && ( this.year == yearScore.getYear()) ;
+    public boolean equals(Object obj) {
+        if (obj instanceof YearScore) {
+        	YearScore yearScore = (YearScore) obj;
+            return (this.score.get() == yearScore.getScore().get())
+                    && ( this.year.get() == yearScore.getYear().get()) ;
         }
         return false;
     }
-    
+
+
+    /** writable methods implementation **/
+	@Override
+	public int compareTo(YearScore yearScore) {
+		return equals(yearScore) ? 1 : -1 ;
+	}
+
+	@Override
+	public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(this.year.toString());
+        dataOutput.writeUTF(this.score.toString());
+	}
+
+	@Override
+	public void readFields(DataInput dataInput) throws IOException {
+        year = new IntWritable(Integer.parseInt(dataInput.readUTF()));
+        score = new IntWritable(Integer.parseInt(dataInput.readUTF()));
+    }
 }
